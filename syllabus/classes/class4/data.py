14e38580f916fd6ea18745b9c1c39cf4a145e8c1
louisenyholmj <louisenyholmj@hotmail.com>
2021-09-29 11:49:09 +0200

    creating freq functions for class 4 etc.

--- syllabus/classes/class4/data.py
+++ syllabus/classes/class4/data.py
@@ -12,6 +12,7 @@ train = dataset["train"]
 print("Examining train set:")
 print(train)
 print(train.features)
+print(train[1]) #sentence number 2, labeled 0 (vs. 1), index 1
 # Examining train set:
 # Dataset({
 #     features: ['sentence', 'label', 'idx'],
@@ -24,15 +25,21 @@ print(train.info.description)
 print("Homepage")
 print(train.info.homepage)
 
+
 print("Examining sentence")
-print(type(train["sentence"]))
-print(type(train["sentence"][0]))
+print(type(train["sentence"])) #list of ALL the sentences (below)
+#print(train["sentence"])
+print(type(train["sentence"][0])) #string
+#print(train["sentence"][0]) #hide new secretions from the parental units
+print(train[0])
 
 
 print("Examining label")
-print(type(train["label"]))
-print(type(train["label"][0]))
-print(set(train["label"]))
+print(type(train["label"])) #list of 0s and 1s (below)
+#print(train["label"])
+print(type(train["label"][0])) #int
+#print(train["label"][0])
+print(set(train["label"])) # {0, 1} 0 = negative, 1 = positive
 
 print("A few samples:")
 for t in range(10):
---
+++ syllabus/classes/class4/frequencies.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+
+def term_freq(doc) -> dict:
+    """
+    Takes in a list of tokens (str) and return a dictionary of term frequency of each token
+    (doc = a list of tokens)
+    """
+    
+    #counts number of cases of the different class types (pos)
+    pos_counts = Counter([token.pos_ for token in doc])
+
+    # For class type tag (pos) and count (both from above),
+    # take the pos tag and the count divided by length to get freq
+    tf_lst = [(pos, count/len(doc)) for pos, count in pos_counts.items()]
+    #convert the list of tuples to a dictionary
+    tf_dict = dict(tf_lst)
+
+    return tf_dict
+
+
+def doc_freq(doc_lst) -> dict:
+    """
+    Takes in a list of documents which each is a list of tokens (str) and return a dictionary of frequencies for each token over all the documents. E.g. {"Aarhus": 20, "the": 2301, ...}
+    """
+    #empty list (can only append to lists, not to counters)
+    all_counters_lst = []
+
+    #Iterating through docs
+    for doc in doc_lst:
+        #append to list a counter with frequencies of pos in each doc
+        all_counters_lst.append(Counter([token.pos_ for token in doc]))
+
+    #Empty counter (.update works on counters, not lists)
+    all_counters = Counter()
+
+    #iterating thorugh counters, updating (=adding)
+    for counter in all_counters_lst:
+        all_counters.update(counter)
+
+    return all_counters
