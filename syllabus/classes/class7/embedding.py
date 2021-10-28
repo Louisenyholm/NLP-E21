8d67d171b75d561b31c58a2e9546b599a7c45255
louisenyholmj <louisenyholmj@hotmail.com>
2021-10-28 18:17:46 +0200

    class 7 some notes not much

--- syllabus/classes/class7/embedding.py
+++ syllabus/classes/class7/embedding.py
@@ -6,6 +6,10 @@ import numpy as np
 
 def gensim_to_torch_embedding(gensim_wv):
     """
+    Input: some kind of model (e.g. Gigaword)
+    Converts gensim word embedding to torch word embedding
+    Returns dict of vocab and associated torch word embeddings
+
     - Add type hints on input and output
     - add function description
     - understand the pad and unk embeddings, add an argument which makes these optional. 
@@ -13,18 +17,21 @@ def gensim_to_torch_embedding(gensim_wv):
     """
     embedding_size = gensim_wv.vectors.shape[1]
 
-    # create unknown and padding embedding
+    # create unknown embedding (for words not seen before - we want to be able to create word embedding for that. We define it arbitrarily as the mean, could also have been all 0s)
+    # and padding embedding (in this case: what we use when we want to pad a sentence - here just a vector of 0s)
     unk_emb = np.mean(gensim_wv.vectors, axis=0).reshape((1, embedding_size))
     pad_emb = np.zeros((1, gensim_wv.vectors.shape[1]))
 
-    # add the new embedding
+    # add the new embedding #combine the three together into a new matrix
     embeddings = np.vstack([gensim_wv.vectors, unk_emb, pad_emb])
 
+    #converting into torch embedding
     weights = torch.FloatTensor(embeddings)
 
+    #the padding index is the last one (-1)
     emb_layer = nn.Embedding.from_pretrained(embeddings=weights, padding_idx=-1)
 
-    # creating vocabulary
+    # creating vocabulary #goes from key to index end up with vocab size of 400002, since we added two new tokens (UNK and PAD)
     vocab = gensim_wv.key_to_index
     vocab["UNK"] = weights.shape[0] - 2
     vocab["PAD"] = emb_layer.padding_idx
--- syllabus/classes/class7/main.py
+++ syllabus/classes/class7/main.py
@@ -3,17 +3,20 @@ from datasets import load_dataset
 import gensim.downloader as api
 
 
-# DATASET
+# DATASET ##Loading the dataset "conllpp", subsetting dataset by train.
+# Tokens are words. ner_tags are integers reflecting NER class (9 classes in total)
 dataset = load_dataset("conllpp")
 train = dataset["train"]
 
 # inspect the dataset
-train["tokens"][:1]
-train["ner_tags"][:1]
+print(train["tokens"][:1])
+print(train["ner_tags"][:1])
 num_classes = train.features["ner_tags"].feature.num_classes
+print(num_classes)
 
 
-# CONVERTING EMBEDDINGS
+# CONVERTING EMBEDDINGS ##getting word embeddings
+#We cannot just use gensim, because gensim returns as np embedding, and we want to return it as torch embedding
 import numpy as np
 
 import torch
@@ -34,29 +37,34 @@ def tokens_to_idx(tokens, vocab=model.key_to_index):
     Ideas to understand this function:
     - Write documentation for this function including type hints for each arguement and return statement
     - What does the .get method do?
-    - Why lowercase?
+    - Why lowercase? In the dict vocab, it is lowercased (it is made in that way)
     """
     return [vocab.get(t.lower(), vocab["UNK"]) for t in tokens]
+    #vocab.get - returns the value for the key (key = t.lower) if it cannot return an idx for the word, then return the idx for the unkown token
+    #vocab = dict
 
 
 # sample batch of 10 sentences
+## create batch of 10 sentences (as example)
 batch_tokens = train["tokens"][:10]
 batch_tags = train["ner_tags"][:10]
-batch_tok_idx = [tokens_to_idx(sent) for sent in batch_tokens]
+batch_tok_idx = [tokens_to_idx(sent) for sent in batch_tokens] #converting sentences to idx
 batch_size = len(batch_tokens)
 
 # compute length of longest sentence in batch
+##to know how much padding is needed
 batch_max_len = max([len(s) for s in batch_tok_idx])
 
 # prepare a numpy array with the data, initializing the data with 'PAD'
 # and all labels with -1; initializing labels to -1 differentiates tokens
 # with tags from 'PAD' tokens
-batch_input = vocab["PAD"] * np.ones((batch_size, batch_max_len))
-batch_labels = -1 * np.ones((batch_size, batch_max_len))
+batch_input = vocab["PAD"] * np.ones((batch_size, batch_max_len)) #batch size down, max length horizontal - filled with the idx of padding token (maybe 400001) = vocab[PAD]
+batch_labels = -1 * np.ones((batch_size, batch_max_len)) #batch size vertical, max length horizontal - filled with -1s (same size as above) = the label for our padding token = -1 (which is not a label used, will later be disregarded - in th LSTM loss function RNN)
 
 # copy the data to the numpy array
+## for each row...
 for i in range(batch_size):
-    tok_idx = batch_tok_idx[i]
+    tok_idx = batch_tok_idx[i] #input the sentence (numbers/idx), and the remainder would be the padding (padding token = 400001)
     tags = batch_tags[i]
     size = len(tok_idx)
 
@@ -65,15 +73,17 @@ for i in range(batch_size):
 
 
 # since all data are indices, we convert them to torch LongTensors
+##Longtensor = as interger (same as as.int) = converting it to this format for later use
 batch_input, batch_labels = torch.LongTensor(batch_input), torch.LongTensor(
     batch_labels
 )
 
 # CREATE MODEL
+##Applying the model
 from LSTM import RNN
 
 model = RNN(
-    embedding_layer=embedding_layer, num_classes=num_classes + 1, hidden_dim_size=256
+    embedding_layer=embedding_layer, output_dim=num_classes + 1, hidden_dim_size=256
 )
 
 # FORWARD PASS
